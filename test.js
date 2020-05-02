059187300ba1d39a5784c7fe49107efdf801d539
Tyler Murphy <tjames7000@gmail.com>
2020-05-02 17:17:28 -0400

    Adds a script for testing and printing results for a range of thread counts.

---
+++ test.js
@@ -0,0 +1,23 @@
+const os = require('os')
+const { execSync } = require('child_process')
+
+const cpuCount = os.cpus().length
+let threads = 0
+
+console.log('| Threads | Duration (s) |')
+console.log('|---------|--------------|')
+
+while (threads++ < cpuCount) {
+    const startTime = Date.now()
+
+    execSync('node index.js', {
+        env: {
+            THREADS: threads
+        },
+        encoding: 'utf8',
+    })
+
+    const duration = Date.now() - startTime
+
+    console.log(`| ${threads.toString().padStart(7, ' ')} | ${Math.round(duration / 1e3).toString().padStart(12, ' ')} |`)
+}
